f6ceed6b14c50b155f73e6f9c4b12ff27c19aaf1
Samuel Hudson <samuel.hudson@brightserve.com>
2016-09-20 11:21:59 +0100

    Initial F1app

--- app/app.js
+++ app/app.js
@@ -1,14 +1,15 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('F1FeederApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+
+  'F1FeederApp.controllers',
+  'F1FeederApp.services'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+  $routeProvider.
+  	when("/drivers", {templateUrl: "partials/drivers.html", controller: "driversController"}).
+	when("/drivers/:id", {templateUrl: "partials/driver.html", controller: "driverController"}).
+	otherwise({redirectTo: '/drivers'});
+}])
