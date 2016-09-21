3ddd4ff8ec573dfd8e71c5e1b40002e6173b8491
Samuel Hudson <samuel.hudson@brightserve.com>
2016-09-21 11:56:03 +0100

    Finishing season section.

--- app/js/services/services.js
+++ app/js/services/services.js
@@ -2,11 +2,12 @@ angular.module('F1FeederApp.services', []).
   factory('ergastAPIservice', function($http) {
 
     var ergastAPI = {};
+    
 
-    ergastAPI.getDrivers = function() {
+    ergastAPI.getDrivers = function(year) {
       return $http({
         method: 'JSONP', 
-        url: 'http://ergast.com/api/f1/2013/driverStandings.json?callback=JSON_CALLBACK'
+        url: 'http://ergast.com/api/f1/'+year+'/driverStandings.json?callback=JSON_CALLBACK'
       });
     }
 
@@ -17,10 +18,21 @@ angular.module('F1FeederApp.services', []).
       });
     }
 
-    ergastAPI.getDriverRaces = function(id) {
+    ergastAPI.getDriverRaces = function(year, id) {
       return $http({
         method: 'JSONP',
-        url: 'http://ergast.com/api/f1/2013/drivers/' + id + '/results.json?callback=JSON_CALLBACK'
+        url: 'http://ergast.com/api/f1/'+ year +'/drivers/'+ id +'/results.json?callback=JSON_CALLBACK'
+      });
+    }
+
+    ergastAPI.getSeason = function(id) {
+      var url = 'http://ergast.com/api/f1/seasons.json?limit=1000&callback=JSON_CALLBACK';
+      if(id != undefined){
+        url = 'http://ergast.com/api/f1/drivers/' + id + '/seasons.json?limit=1000&callback=JSON_CALLBACK';
+      }
+      return $http({
+        method: 'JSONP',
+        url: url
       });
     }
 
